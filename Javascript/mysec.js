4454f0f50840db2a3ebdff933f160b3995a0a097
Invinciblek2003 <abhijitkulkarni896@gmail.com>
2023-08-28 23:07:57 +0530

    js variables

---
+++ Javascript/mysec.js
@@ -0,0 +1,13 @@
+// Variable is container to store data
+//4 ways-automatic,let,const,var
+//Syntax
+//let/var/const variable name; -declaration
+//let/const/var variable name=value; -decl+def.
+
+let a;//Declare
+console.log(a);
+
+a=10;
+console.log(a);
+
+//variable names rules:Space not allowed,do not start with letter
